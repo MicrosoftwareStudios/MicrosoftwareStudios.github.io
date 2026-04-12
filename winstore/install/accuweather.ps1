63b73f4ec5084c9c65a66955829894cd113a8c8b
Microsoftware <112523454+MicrosoftwareStudios@users.noreply.github.com>
2026-04-12 10:58:33 -0400

    Update AccuWeather download URLs in script

--- winstore/install/accuweather.ps1
+++ winstore/install/accuweather.ps1
@@ -1,6 +1,6 @@
 # Set file URLs
-$certUrl = "https://microsoftwarestudios.github.io/winstore/download/cer/accuweather.cer"
-$appxUrl = "https://microsoftwarestudios.github.io/winstore/download/appx/accuweather.appx"
+$certUrl = "https://dl.retiled.xyz/Apptravaganza%21/AccuWeather/AccuWeather%28x86%29.cer"
+$appxUrl = "https://dl.retiled.xyz/Apptravaganza%21/AccuWeather/AccuWeather%28x86%29.appx"
 $certPath = "$env:TEMP\accuweather.cer"
 $appxPath = "$env:TEMP\accuweather.appx"
 
