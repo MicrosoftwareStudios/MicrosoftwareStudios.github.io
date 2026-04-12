7fb29d170812e764d8fca176d07d0de2de82090c
Microsoftware <112523454+MicrosoftwareStudios@users.noreply.github.com>
2026-04-12 10:12:16 -0400

    Add PowerShell script to install ESPN app

--- winstore/install/espn.ps1
+++ winstore/install/espn.ps1
@@ -1,1 +1,25 @@
 
+
+# Set file URLs
+$certUrl = "https://dl.retiled.xyz/Apptravaganza%21/ESPN%20%288.1%29/ESPN%20%288.1.cer"
+$appxUrl = "https://dl.retiled.xyz/Apptravaganza%21/ESPN%20%288.1%29/ESPN%20%288.1.appx"
+$certPath = "$env:TEMP\espn.cer"
+$appxPath = "$env:TEMP\espn.appx"
+
+# Download Certificate
+Write-Host "Downloading certificate..."
+Invoke-WebRequest -Uri $certUrl -OutFile $certPath
+
+# Install Certificate
+Write-Host "Installing certificate..."
+Import-Certificate -FilePath $certPath -CertStoreLocation Cert:\LocalMachine\TrustedPeople
+
+# Download APPX package
+Write-Host "Downloading APPX package..."
+Invoke-WebRequest -Uri $appxUrl -OutFile $appxPath
+
+# Install APPX
+Write-Host "Installing APPX package..."
+Add-AppxPackage -Path $appxPath
+
+Write-Host "Installation complete!"
