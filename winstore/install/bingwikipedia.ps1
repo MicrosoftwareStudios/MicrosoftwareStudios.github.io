3724649fd6f221cec0914d9c147d7dd5b927620c
Microsoftware <112523454+MicrosoftwareStudios@users.noreply.github.com>
2026-04-12 20:13:22 -0400

    Add PowerShell script to install Bing Wikipedia Browser

---
+++ winstore/install/bingwikipedia.ps1
@@ -0,0 +1,26 @@
+
+
+
+# Set file URLs
+$certUrl = "https://dl.retiled.xyz/Apptravaganza%21/Bing%20Wikipedia%20Browser/x86/Microsoft.BingWikipediaBrowser_1.0.0.108_x86__8wekyb3d8bbwe.cer"
+$appxUrl = "https://dl.retiled.xyz/Apptravaganza%21/Bing%20Wikipedia%20Browser/x86/Microsoft.BingWikipediaBrowser_1.0.0.108_x86__8wekyb3d8bbwe.appx"
+$certPath = "$env:TEMP\cut-the-rope.cer"
+$appxPath = "$env:TEMP\cut-the-rope.appx"
+
+# Download Certificate
+Write-Host "Downloading certificate..."
+Invoke-WebRequest -Uri $certUrl -OutFile $certPath
+
+# Install Certificate
+Write-Host "Installing certificate..."
+Import-Certificate -FilePath $certPath -CertStoreLocation Cert:\LocalMachine\TrustedPeople
+
+# Download APPX package
+Write-Host "Downloading APPX package..."
+Invoke-WebRequest -Uri $appxUrl -OutFile $appxPath
+
+# Install APPX
+Write-Host "Installing APPX package..."
+Add-AppxPackage -Path $appxPath
+
+Write-Host "Installation complete!"
